1b436a61b50504be3943166ce4e12e1b710a74c0
LizaMatyukhina <liza.matyukhina2000@gmail.com>
2018-12-01 09:35:19 +0700

    start

---
+++ case6.py
@@ -0,0 +1,15 @@
+def get_num_hexagons():
+    #TODO
+
+
+def get_color_choice():
+    #TODO
+
+def draw_hexagon(x, y, side_len, color):
+    #TODO
+
+def main():
+    #TODO
+
+if __name__ == "__msin__":
+    main()
